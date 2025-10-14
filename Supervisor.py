59f49f4192b83d526124d4adc4bc5b3e320e9b7b
Anger Armand <aanger3@gmail.com>
2025-10-14 10:11:41 +0200

    FIX : use with to avoid leak with open file

--- Supervisor.py
+++ Supervisor.py
@@ -127,72 +127,73 @@ class Supervisor:
         self.start(processus_names, all)
 
     def reread(self):
-        try:
-            with open(self.path_to_config, 'r') as file:
-                config_data = yaml.safe_load(file)
-        except FileNotFoundError:
-            print(f"Error: Can't REREAD file '{self.path_to_config}' not found.")
-            return
-        except yaml.YAMLError as e:
-            print(f"Error:  Can't REREAD :  while parsing YAML in '{self.path_to_config}': {e}")
-            return
-        
-        if not isinstance(config_data, dict) or "programs" not in config_data:
-            print("Error :  Can't REREAD : configuration file must have a section programs:")
-            return
-
-        modification = False
-        for name, config in config_data["programs"].items():
+        with self.lock:
             try:
-                if name in self.processus_list:
-                    if config == self.processus_list[name].raw_config:
-                        self.new_processus_list[name] = self.processus_list[name]
+                with open(self.path_to_config, 'r') as file:
+                    config_data = yaml.safe_load(file)
+            except FileNotFoundError:
+                print(f"Error: Can't REREAD file '{self.path_to_config}' not found.")
+                return
+            except yaml.YAMLError as e:
+                print(f"Error:  Can't REREAD :  while parsing YAML in '{self.path_to_config}': {e}")
+                return
+            
+            if not isinstance(config_data, dict) or "programs" not in config_data:
+                print("Error :  Can't REREAD : configuration file must have a section programs:")
+                return
+
+            modification = False
+            for name, config in config_data["programs"].items():
+                try:
+                    if name in self.processus_list:
+                        if config == self.processus_list[name].raw_config:
+                            self.new_processus_list[name] = self.processus_list[name]
+                        else:
+                            if name not in self.old_processus_to_stop:
+                                self.old_processus_to_stop.append(name)
+                            task = Task.create(name, config)
+                            self.new_processus_list[name] = task
+                            self.new_processus_to_start[name] = task
+                            self.new_processus_list[name].raw_config = config
+                            self.processus_list[name].raw_config = config
+                            modification = True
+                            print(f"{name}: changed")
                     else:
-                        if name not in self.old_processus_to_stop:
-                            self.old_processus_to_stop.append(name)
                         task = Task.create(name, config)
                         self.new_processus_list[name] = task
                         self.new_processus_to_start[name] = task
-                        self.new_processus_list[name].raw_config = config
-                        self.processus_list[name].raw_config = config
                         modification = True
-                        print(f"{name}: changed")
-                else:
-                    task = Task.create(name, config)
-                    self.new_processus_list[name] = task
-                    self.new_processus_to_start[name] = task
-                    modification = True
-                    print(f"{name}: available")
-            except Exception as e:
-                print(f"Error :  Can't REREAD : in task '{name}': {e}")
-                return
-        if modification == False:
-            print(f"No config updates to processes")
+                        print(f"{name}: available")
+                except Exception as e:
+                    print(f"Error :  Can't REREAD : in task '{name}': {e}")
+                    return
+            if modification == False:
+                print(f"No config updates to processes")
 
 
     def update(self):
         autostart = []
+        with self.lock:
+            # Stop process delete from config
+            if not self.new_processus_list == {}:
+                for name, processus in self.processus_list.items():
+                    if name not in self.new_processus_list:
+                        processus.stop()
 
-        # Stop process delete from config
-        if not self.new_processus_list == {}:
-            for name, processus in self.processus_list.items():
-                if name not in self.new_processus_list:
-                    processus.stop()
+                # Stop process 
+                if self.old_processus_to_stop:
+                    self.stop(self.old_processus_to_stop)   
 
-            # Stop process 
-            if self.old_processus_to_stop:
-                self.stop(self.old_processus_to_stop)   
+                # Autostart of ew process
+                for name, new_processus in self.new_processus_to_start.items():
+                    if new_processus.autostart == True:
+                        autostart.append(name)
+                self.processus_list = self.new_processus_list
+                self.old_processus_to_stop = []
+                self.new_processus_to_start = {}
+                self.new_processus_list = {}
 
-            # Autostart of ew process
-            for name, new_processus in self.new_processus_to_start.items():
-                if new_processus.autostart == True:
-                    autostart.append(name)
-            self.processus_list = self.new_processus_list
-            self.old_processus_to_stop = []
-            self.new_processus_to_start = {}
-            self.new_processus_list = {}
-
-            self.start(autostart)
+                self.start(autostart)
 
     def supervise(self, event: Event):
         with self.lock:
